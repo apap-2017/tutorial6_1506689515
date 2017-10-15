0342542a60d00f5bcdfddf99f5c750b1fae4b93b
jihanadibah <jhnadibah@gmail.com>
2017-10-15 21:28:40 +0700

    tutor 6

---
+++ tutorial06/src/main/java/com/example/Tutorial06Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial06Application.class, args);
+	}
+}
